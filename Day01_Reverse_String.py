69d0280a6ee6291a12034f5feb6317738e384e55
NEERAJ KUMAR <dnk7363@gmail.com>
2025-10-08 01:13:52 +0530

    Create Day01_Reverse_String.py

---
+++ Day01_Reverse_String.py
@@ -0,0 +1,26 @@
+"""
+LeetCode 344. Reverse String
+Link: https://leetcode.com/problems/reverse-string/
+
+Write a function that reverses a string. The input string is given as an array
+of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+"""
+
+# Your solution code goes below the comment
+class Solution(object):
+    def reverseString(self, s):
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left += 1
+            right -= 1
