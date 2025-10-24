d37ae9718ad4acbf639e372e62d04e38ae0f9da8
NEERAJ KUMAR <dnk7363@gmail.com>
2025-10-24 21:23:57 +0530

    Day17
    Implement solution for LeetCode Problem #122 to calculate maximum profit from stock transactions.

---
+++ Day17_ Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,29 @@
+"""
+LeetCode Problem #122: Best Time to Buy and Sell Stock II
+
+Problem Statement:
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. 
+However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+
+Note:
+- You can make multiple transactions (buy and sell multiple times).
+- You cannot engage in multiple transactions simultaneously (you must sell the stock before you buy again).
+"""
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        total_profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                total_profit += prices[i] - prices[i-1]
+        return total_profit
+
+Time Complexity: O(n)
+Space Complexity: O(1)
